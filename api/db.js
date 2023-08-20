f8d164426382a559c671b0785aa0b02ec99eb972
Paulo Vargas <paulotomegomesdevargas@hotmail.com>
2023-08-20 20:40:20 -0300

    Ajustes

--- api/db.js
+++ api/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql";
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Nfe934122*",
+    password: "senha123*",
     database: "crud2"
 })
--- frontend/src/styles/global.js
+++ frontend/src/styles/global.js
@@ -9,7 +9,11 @@ const Global = createGlobalStyle`
 }
 
 body {
-    width: 
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    background-color: #f2f2f2;
 }
 `;
 
